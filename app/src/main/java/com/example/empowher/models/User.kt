40cd1abc185dbbda893453f6801542e6cbbdde68
estiakahamed01 <tasin220116@diit.edu.bd>
2026-09-02 23:36:15 +0600

    Add Authentication

---
+++ app/src/main/java/com/example/empowher/models/User.kt
@@ -0,0 +1,10 @@
+package com.example.empowher.models
+
+data class User(
+    val uid: String = "",
+    val name: String = "",
+    val email: String = "",
+    val phone: String = "",
+    val dob: String = "",
+    val gender: String = ""
+)
