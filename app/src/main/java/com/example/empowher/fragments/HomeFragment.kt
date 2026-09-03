53f591d20dd8dee15ea9c61dd6271d0fadc4878f
estiakahamed01 <tasin220116@diit.edu.bd>
2026-09-03 23:08:16 +0600

    Added Home Fragment

--- app/src/main/java/com/example/empowher/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/empowher/fragments/HomeFragment.kt
@@ -4,11 +4,38 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.example.empowher.R
+import com.example.empowher.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.layoutSos.setOnClickListener {
+            Toast.makeText(context, "SOS Triggered!", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.ivNotification.setOnClickListener {
+            Toast.makeText(context, "Notifications clicked", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
