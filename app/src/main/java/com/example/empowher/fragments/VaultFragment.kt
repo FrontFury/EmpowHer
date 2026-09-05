a8ca380450b0acc9e3e8c6df37741d8c56319a02
estiakahamed01 <tasin220116@diit.edu.bd>
2026-09-06 00:08:54 +0600

    Added Vault Page

--- app/src/main/java/com/example/empowher/fragments/VaultFragment.kt
+++ app/src/main/java/com/example/empowher/fragments/VaultFragment.kt
@@ -5,10 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.empowher.R
+import com.example.empowher.databinding.FragmentVaultBinding
 
 class VaultFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_vault, container, false)
+    private var _binding: FragmentVaultBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentVaultBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
+
