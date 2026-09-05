301cbe7aaf3bbff06d9ed17a65dc633651270161
estiakahamed01 <tasin220116@diit.edu.bd>
2026-09-06 00:03:45 +0600

    Added Cases Page

--- app/src/main/java/com/example/empowher/fragments/CasesFragment.kt
+++ app/src/main/java/com/example/empowher/fragments/CasesFragment.kt
@@ -5,10 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.empowher.R
+import com.example.empowher.databinding.FragmentCasesBinding
 
 class CasesFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_cases, container, false)
+    private var _binding: FragmentCasesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCasesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
+
